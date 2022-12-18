885927de554bf915af1674f52b472b5c7c8b1ea9
Jason Mai <maijason112102@gmail.com>
2022-12-18 13:27:42 -0500

    stuff

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ import LogoS from '../../assets/images/coolJ.gif'
 import Valorant from '../../assets/images/ValorantGamerGif.gif';
 import animeProgramming from '../../assets/images/animeprogramming.gif'
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
-import resume from '../../assets/images/Fall22ApplyingResume.pdf';
+import resume from '../../assets/images/Resume_Winter22_Jason_Mai.pdf';
 
 
 const Home = () => {
